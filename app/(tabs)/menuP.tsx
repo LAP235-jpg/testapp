3e5f17586f7d8c747110509f64b7f7021a05b0c0
LAP235-jpg <lapa86002@gmail.com>
2026-06-23 09:39:37 -0300

    Refactor code structure for improved readability and maintainability

--- app/(tabs)/menuP.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Image, View } from 'react-native';
-export default function MenuP() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#5d5d5d' }}>
-      <Image source={require('../../assets/images/TALKPUP.png')} />
-    </View>
-  );
-}
